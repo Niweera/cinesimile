b8e14f877767063a2903f277c3c06f4771324846
Niweera <w.nipuna@gmail.com>
2020-09-19 13:57:48 +0530

    Init React Project for CineSimile

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Someday hopefully better late than never, this is going to be the home
+          of CineSimile. A platform where you can find similar movies easily.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
