68437dda3ce90762be9d8f3b40c1eeff164ceadd
Pindev <Gitlabapin82y@gmail.com>
2020-10-01 19:54:44 +0700

    design home page and added movie search input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Routes from "./routes";
 import "./app.scss";
+import './app.scss';
 
 const App = () => {
   return <Routes />;
--- src/components/Home/introduction_component.js
+++ src/components/Home/introduction_component.js
@@ -4,11 +4,17 @@ import { Button } from 'carbon-components-react';
 const IntroComp = () => {
   return (
     <div className="intro-comp">
+    <img src={process.env.PUBLIC_URL + '/film.jpg'} />
+    <div className="top-layouts">
       <h1 className="landing-page__heading">Hello, world!</h1>
       <p>Welcome to CineSmile. We are platform to find similar movies easily. 
         We use sophisticated machine learning models to find similarities between movies and provide you with close matches.
       </p>
-      <Button>Learn more</Button>
+      <div className="search-film">
+      <input placeholder="Search Film"></input>
+      </div>
+      <Button>Find movies now!</Button>
+    </div>
     </div>
   );
 };
