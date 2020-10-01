3057e1ec8928df77680adef64bf9c906a7fa69c8
VinuriBandara <vinugayanthika@gmail.com>
2020-10-01 19:45:56 +0530

    Update movie cards

--- src/components/Movie_Cards/index.js
+++ src/components/Movie_Cards/index.js
@@ -1,146 +1,26 @@
-import React from 'react';
-import { CheckmarkFilled16 } from '@carbon/icons-react';
-import {
-  StructuredListWrapper,
-  StructuredListHead,
-  StructuredListBody,
-  StructuredListRow,
-  StructuredListInput,
-  StructuredListCell,
-} from '../StructuredList';
-import StructuredListSkeleton from '../StructuredList/StructuredList.Skeleton';
-import { settings } from 'carbon-components';
-import mdx from './StructuredList.mdx';
+import React from "react";
+import "./index.scss";
 
-const { prefix } = settings;
+import {
+  Tile
+} from '../Tile';
 
 export default {
-  title: 'StructuredList',
+  title: 'Tile',
+  decorators: [withKnobs],
+}
 
-  parameters: {
-    component: StructuredListWrapper,
-    docs: {
-      page: mdx,
-    },
-    subcomponents: {
-      StructuredListHead,
-      StructuredListBody,
-      StructuredListRow,
-      StructuredListInput,
-      StructuredListCell,
-    },
-  },
+export const Default = () => {
+  const regularProps = props.regular();
+  return <Tile {...regularProps}>Movie one</Tile>;
 };
 
-export const Simple = () => (
-  <StructuredListWrapper>
-    <StructuredListHead>
-      <StructuredListRow head>
-        <StructuredListCell head>ColumnA</StructuredListCell>
-        <StructuredListCell head>ColumnB</StructuredListCell>
-        <StructuredListCell head>ColumnC</StructuredListCell>
-      </StructuredListRow>
-    </StructuredListHead>
-    <StructuredListBody>
-      <StructuredListRow>
-        <StructuredListCell noWrap>Row 1</StructuredListCell>
-        <StructuredListCell>Row 1</StructuredListCell>
-        <StructuredListCell>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dui
-          magna, finibus id tortor sed, aliquet bibendum augue. Aenean posuere
-          sem vel euismod dignissim. Nulla ut cursus dolor. Pellentesque
-          vulputate nisl a porttitor interdum.
-        </StructuredListCell>
-      </StructuredListRow>
-      <StructuredListRow>
-        <StructuredListCell noWrap>Row 2</StructuredListCell>
-        <StructuredListCell>Row 2</StructuredListCell>
-        <StructuredListCell>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dui
-          magna, finibus id tortor sed, aliquet bibendum augue. Aenean posuere
-          sem vel euismod dignissim. Nulla ut cursus dolor. Pellentesque
-          vulputate nisl a porttitor interdum.
-        </StructuredListCell>
-      </StructuredListRow>
-    </StructuredListBody>
-  </StructuredListWrapper>
-);
-
-Simple.parameters = {
+Default.parameters = {
   info: {
     text: `
-        Structured Lists group content that is similar or related, such as terms or definitions.
+        Movie title
       `,
   },
 };
 
-export const Selection = () => {
-  const structuredListBodyRowGenerator = (numRows) => {
-    return Array.apply(null, Array(numRows)).map((n, i) => (
-      <StructuredListRow label key={`row-${i}`}>
-        <StructuredListCell>Row {i}</StructuredListCell>
-        <StructuredListCell>Row {i}</StructuredListCell>
-        <StructuredListCell>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dui
-          magna, finibus id tortor sed, aliquet bibendum augue. Aenean posuere
-          sem vel euismod dignissim. Nulla ut cursus dolor. Pellentesque
-          vulputate nisl a porttitor interdum.
-        </StructuredListCell>
-        <StructuredListInput
-          id={`row-${i}`}
-          value={`row-${i}`}
-          title={`row-${i}`}
-          name="row-0"
-          defaultChecked={!i || null}
-        />
-        <StructuredListCell>
-          <CheckmarkFilled16
-            className={`${prefix}--structured-list-svg`}
-            aria-label="select an option">
-            <title>select an option</title>
-          </CheckmarkFilled16>
-        </StructuredListCell>
-      </StructuredListRow>
-    ));
-  };
-  return (
-    <StructuredListWrapper selection>
-      <StructuredListHead>
-        <StructuredListRow head>
-          <StructuredListCell head>ColumnA</StructuredListCell>
-          <StructuredListCell head>ColumnB</StructuredListCell>
-          <StructuredListCell head>ColumnC</StructuredListCell>
-          <StructuredListCell head>{''}</StructuredListCell>
-        </StructuredListRow>
-      </StructuredListHead>
-      <StructuredListBody>
-        {structuredListBodyRowGenerator(4)}
-      </StructuredListBody>
-    </StructuredListWrapper>
-  );
-};
 
-Selection.parameters = {
-  info: {
-    text: `
-    Structured Lists with selection allow a row of list content to be selected.
-  `,
-  },
-};
-
-export const Skeleton = () => (
-  <div style={{ width: '800px' }}>
-    <StructuredListSkeleton />
-    <StructuredListSkeleton />
-  </div>
-);
-
-Skeleton.storyName = 'skeleton';
-
-Skeleton.parameters = {
-  info: {
-    text: `
-        Placeholder skeleton state to use when content is loading.
-      `,
-  },
-};
