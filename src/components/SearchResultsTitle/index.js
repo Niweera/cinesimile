43b1c3b55d9d8eb8a5f8429ed4c6b9b3012a8900
Bojan Milicev <boyan93@gmail.com>
2020-10-17 13:41:41 +0200

    Add card results

--- src/components/SearchResultsTitle/index.js
+++ src/components/SearchResultsTitle/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../../App.css";
 
 const SearchResultsTitle = ({ title }) => {
-  return <h2 className="search-results__title">Movies like: {title}</h2>;
+  return <h2 className="search-query">Movies like: {title}</h2>;
 };
 
 export default SearchResultsTitle;
