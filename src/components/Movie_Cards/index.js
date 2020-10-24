98d55f238b2669becd6e47c58f4ee71bfb93c7f1
Bojan Milicev <boyan93@gmail.com>
2020-10-24 11:13:14 +0200

    Fix search bugs, make card prettier

--- src/components/Movie_Cards/index.js
+++ src/components/Movie_Cards/index.js
@@ -3,10 +3,14 @@ import React from "react";
 export const MovieCard = ({ title, description }) => {
   return (
     <div className="search-results__card">
-      <img alt="movie poster" src="https://dummyimage.com/320x170/bfbfbf/fff" />
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <button>Go somewhere</button>
+      <img
+        className="search-results__card-poster"
+        alt="movie poster"
+        src="https://dummyimage.com/320x170/bfbfbf/fff"
+      />
+      <h3 className="search-results__card-title">{title}</h3>
+      <p className="search-results__card-description">{description}</p>
+      <button className="search-results__card-button">Go somewhere</button>
     </div>
   );
 };
