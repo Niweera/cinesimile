9c9f380b7f68643b35f35e7ce36bec576b8b33ef
SanJay <sanjaya@zilingo.com>
2021-10-13 21:13:40 +0530

    fixed key label var keyword

--- src/components/Navbar/sidenavbar.js
+++ src/components/Navbar/sidenavbar.js
@@ -12,7 +12,7 @@ const Sidenavbar = () => {
         <HeaderPanel aria-label="Header Panel" className="header-side">
         <Switcher aria-label="Switcher Container">
         {navbarItems.map( (item,index)=>{
-           var keyLabel=index+item.title+"sideBar";
+           let keyLabel=index+item.title+"sideBar";
                                 return (
 
                                    <React.Fragment key={index+"div"+item.title}><SwitcherItem aria-label={keyLabel} href={item.url} className="navbar-items"  key={index+"sidebar"}>{item.title}</SwitcherItem> <SwitcherDivider /></React.Fragment >
