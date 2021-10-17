eafa419f22845c4a21eb3d8d903cdbff3dadd896
AsithaIndrajith <asithaindrajithk9@gmail.com>
2021-10-17 13:25:56 +0530

    add image alt in Movie_Cards

--- src/components/Movie_Cards/index.js
+++ src/components/Movie_Cards/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export const MovieCard = ({ title, description }) => {
   return (
     <div className="search-results__card">
-      <img src="https://dummyimage.com/320x170/bfbfbf/fff" />
+      <img alt='dummyimage' src="https://dummyimage.com/320x170/bfbfbf/fff" />
       <h3>{title}</h3>
       <p>{description}</p>
       <button>Go somewhere</button>
